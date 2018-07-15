6b74eb0db75fd950383b05aee4e39ec7c353f685
Tao <werewaswas@live.com>
2018-07-15 02:59:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Write your solution in this file!
 var customerName = 'bob';
 
-let bestCustomer
+var bestCustomer
 
 function upperCaseCustomerName() {
   customerName = customerName.toUpperCase();
